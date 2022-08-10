8958d1cf4f70d86b504bfc92c1e34a88e0d71b9b
doanduyhien203 <doanhien203302@gmail.com>
2022-08-10 17:57:06 +0700

    dsfs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component';
 
 
 @Component({
@@ -7,9 +8,24 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
-  name = 'Angular';
+//  @ViewChild('toggleComp') toggleComp: ToggleComponent;// truyen string
+  @ViewChild(ToggleComponent, {static: true}) toggleComp: ToggleComponent; 
+  @ViewChild('toggleButton', {static: true} ) toggleBtn: ElementRef<HTMLButtonElement>;
+  @ViewChild('nameInput',{static:true}) nameInput: ElementRef<HTMLInputElement>;
   title = 'test-test';
+  name = "Angular";
   currentProgress = 30;
   isChecked = true;
+  ngOnInit(){
+    setTimeout (() => {
+    this.nameInput.nativeElement.focus()
+  }, 3000)
+    console.log('onInit',this.toggleComp, this.toggleBtn); 
+  }
+  ngAfterViewInit(){
+    console.log(this.toggleComp);
+//      apiCall.subscribe(()=> {
+//       this.toggleComp.toggle();
+//    })
+  }
 }
